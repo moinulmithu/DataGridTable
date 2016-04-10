1acfe91bd1b4bddceb977cbaf945c437f6f883e1
Moinul Islam <moinulmithu@hotmail.com>
2016-04-11 00:23:21 +0600

    Data Table enabled

---
+++ MVC.DataTable/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC.DataTable.Startup))]
+namespace MVC.DataTable
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
