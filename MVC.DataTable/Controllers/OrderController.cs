30151331af7191e854463cee76229c0f29d85dc1
Moinul Islam <moinulmithu@hotmail.com>
2016-04-14 12:15:25 +0600

    DataTable enabled in partial page

---
+++ MVC.DataTable/Controllers/OrderController.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC.DataTable.Models;
+
+namespace MVC.DataTable.Controllers
+{
+    public class OrderController : Controller
+    {
+        DropdownVM viewModel = new DropdownVM();
+        NORTHWNDEntities db = new NORTHWNDEntities();
+        // GET: Order
+        public ActionResult Index()
+        {
+            ViewBag.Order = new SelectList(db.Orders, "CustomerID", "CustomerID");
+            return View();
+        }
+
+        public ActionResult LoadOrders(string custId)
+        {
+            var orderList = db.Orders.Where(x => x.CustomerID == custId).ToList();
+            return PartialView(orderList);
+        }
+        // GET: Order/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Order/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Order/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Order/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Order/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Order/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Order/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ MVC.DataTable/Models/DropdownVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.DataTable.Models
+{
+    public class DropdownVM
+    {
+        public List<Customer> Customers { get; set; }
+        public string CustomerName { get; set; }
+    }
+}
