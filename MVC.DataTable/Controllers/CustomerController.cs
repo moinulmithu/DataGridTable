f3c2e6e34ea2382182c50f3958d19dff795215eb
Moinul Islam <moinulmithu@hotmail.com>
2016-05-05 00:39:26 +0600

    Customer Searching

---
+++ MVC.DataTable/Controllers/CustomerController.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC.DataTable.Models;
+
+namespace MVC.DataTable.Controllers
+{
+    public class CustomerController : Controller
+    {
+        NORTHWNDEntities db = new NORTHWNDEntities();
+        // GET: Customer
+        public ActionResult Index(CustomerSearchModel customerSearchModel)
+        {
+            var result = db.Customers.AsQueryable();
+            if (customerSearchModel != null)
+            {
+                if (!string.IsNullOrEmpty(customerSearchModel.CustomerID))
+                {
+                    result = result.Where(x => x.CustomerID.Contains(customerSearchModel.CustomerID));
+                }
+                else if (!string.IsNullOrEmpty(customerSearchModel.ContactName))
+                {
+                    result = result.Where(x => x.ContactName.Contains(customerSearchModel.ContactName));
+                }
+                else if (!string.IsNullOrEmpty(customerSearchModel.City))
+                {
+                    result = result.Where(x => x.City.Contains(customerSearchModel.City));
+                }
+                else if (!string.IsNullOrEmpty(customerSearchModel.Country))
+                {
+                    result = result.Where(x => x.Country.Contains(customerSearchModel.Country));
+                }
+                else
+                {
+                    result.ToList();
+                }
+            }
+            return View(result);
+        }
+
+        public IQueryable<Customer> GetCustomers(CustomerSearchModel customerSearchModel)
+        {
+            var result = db.Customers.AsQueryable();
+            if (customerSearchModel != null)
+            {
+                if (!string.IsNullOrEmpty(customerSearchModel.CustomerID))
+                {
+                    result = result.Where(x => x.CustomerID.Contains(customerSearchModel.CustomerID));
+                }
+                if (!string.IsNullOrEmpty(customerSearchModel.ContactName))
+                {
+                    result = result.Where(x => x.ContactName.Contains(customerSearchModel.ContactName));
+                }
+                if (!string.IsNullOrEmpty(customerSearchModel.City))
+                {
+                    result = result.Where(x => x.City.Contains(customerSearchModel.City));
+                }
+                if (!string.IsNullOrEmpty(customerSearchModel.Country))
+                {
+                    result = result.Where(x => x.Country.Contains(customerSearchModel.Country));
+                }
+            }
+            return result;
+        } 
+        // GET: Customer/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Customer/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Customer/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Customer/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Customer/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Customer/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Customer/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ MVC.DataTable/Models/CustomerSearchModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC.DataTable.Models
+{
+    public class CustomerSearchModel
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public CustomerSearchModel()
+        {
+            this.Orders = new HashSet<Order>();
+            this.CustomerDemographics = new HashSet<CustomerDemographic>();
+        }
+
+        public string CustomerID { get; set; }
+        public string CompanyName { get; set; }
+        public string ContactName { get; set; }
+        public string ContactTitle { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Region { get; set; }
+        public string PostalCode { get; set; }
+        public string Country { get; set; }
+        public string Phone { get; set; }
+        public string Fax { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Order> Orders { get; set; }
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<CustomerDemographic> CustomerDemographics { get; set; }
+    }
+}
